174788eea65368cdc655b6c790b36f829f344527
jjrohrer <jj.bitbucket@rohrer.org>
2020-11-17 07:18:08 -0500

    handle dynamic card registratoin

--- src/Http/Controllers/admin/DashboardController.php
+++ src/Http/Controllers/admin/DashboardController.php
@@ -10,4 +10,24 @@ class DashboardController extends \TallAndSassy\PageGuide\Http\Controllers\Admin
     {
         return view(static::viewRef, ['LastName' => "Deis"]);
     }
+
+    private array $stack = [];
+    private static array $mes;
+    public static function singleton(string $handle) : DashboardController
+    {
+        if (! isset(static::$mes[$handle])) {
+            static::$mes[$handle] = new DashboardController();
+        }
+
+        return static::$mes[$handle];
+    }
+
+    public function push($objCard) : self {
+        $this->stack[] = $objCard;
+        return $this;
+    }
+
+    public function getAll(): array {
+        return $this->stack;
+    }
 }
